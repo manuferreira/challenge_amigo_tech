f122b7414cd51262291272dab977684187db876f
manuferreira <manussa.fsantos@gmail.com>
2024-01-26 16:29:41 -0300

    removed the '*' it was giving an error while calling the object

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,22 +1,22 @@
 from selenium.webdriver.common.by import By
-from base_page import BasePage
+from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    def __init__(self, webdriver):
-        super().__init__(webdriver)
+    def __init__(self, driver):
+        super().__init__(driver)
         self.username_field = (By.ID, 'user-name')
         self.password_field = (By.ID, 'password')
         self.login_button = (By.ID, 'login-button')
 
     def set_username(self, username):
-        self.set(*self.username_field, username)
+        self.set(self.username_field, username)
 
     def set_password(self, password):
-        self.set(*self.password_field, password)
+        self.set(self.password_field, password)
 
     def click_to_login(self):
-        self.click(*self.login_button)
+        self.click(self.login_button)
 
     def login(self, username, password):
         self.set_username(username)
