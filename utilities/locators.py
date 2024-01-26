631eb3f56400e6f2d08d425dc40187dd731ba88b
manuferreira <manussa.fsantos@gmail.com>
2024-01-26 12:00:27 -0300

    add locators related to the products and cart

--- utilities/locators.py
+++ utilities/locators.py
@@ -7,3 +7,8 @@ class Utilities:
 class BasePageLocators:
     cart_icon = (By.XPATH, '//a[@class="shopping_cart_link"]')
 
+class ProductsLocators:
+    product_button = (By.XPATH, '//div[@class="inventory_item_label"]/a/div[text()="{item_name}"]/../../following-sibling::div//button')
+
+class CartLocators:
+    remove_product_button = (By.XPATH, '//div[@class="cart_item_label"]/a/div[text()="{item_name}"]/../../div[@class="item_pricebar"]//button')
