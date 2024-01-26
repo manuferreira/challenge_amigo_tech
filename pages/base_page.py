b79333797965162537a6b822baa38ed1adb68d76
manuferreira <manussa.fsantos@gmail.com>
2024-01-26 01:31:22 -0300

    add the base page with all common methods to the other pages

---
+++ pages/base_page.py
@@ -0,0 +1,25 @@
+from utilities.locators import BasePageLocators
+
+
+class BasePage:
+    """
+    Essa classe contem metodos comuns a todas as páginas
+    """
+    def __init__(self, webdriver):
+        self.webdriver = webdriver
+
+    def find(self, *locator):
+        return self.webdriver.find_element(*locator)
+
+    def click(self, locator):
+        self.find(*locator).click()
+
+    def set(self, locator, value):
+        # self.find(*locator).clear()
+        self.find(*locator).send_keys(value)
+
+    def get_text(self, *locator):
+        return self.find(*locator).text
+
+    def go_to_cart(self):
+        self.find(BasePageLocators.cart_icon).click()
