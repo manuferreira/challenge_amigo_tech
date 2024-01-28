3cb34f727196b0b9767b3d578b3fe7e4649c1f9c
manuferreira <manussa.fsantos@gmail.com>
2024-01-28 20:58:26 -0300

    update checkout page fixing the functions

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -4,7 +4,7 @@ from utilities.locators import CheckoutLocators
 
 
 class CheckoutPage(BasePage):
-    def __int__(self, driver):
+    def __init__(self, driver):
         super().__init__(driver)
         self.first_name = (By.ID, 'first-name')
         self.last_name = (By.ID, 'last-name')
@@ -13,7 +13,8 @@ class CheckoutPage(BasePage):
         self.error_message = CheckoutLocators.warning_message
         self.item_name_checkout = CheckoutLocators.checkout_item_name
         self.item_price_checkout = CheckoutLocators.checkout_item_price
-        self.finish_checkout = (By.CLASS_NAME, 'summary_subtotal_label')
+        self.subtotal = (By.CLASS_NAME, 'summary_subtotal_label')
+        self.finish_checkout = (By.ID, 'finish')
         self.success_checkout_message = CheckoutLocators.checkout_message
 
     def set_first_name(self, firstname):
@@ -32,18 +33,34 @@ class CheckoutPage(BasePage):
         self.click(self.continue_checkout)
 
     def get_warning_message(self):
-        return self.error_message.text
+        return self.get_text(*self.error_message)
 
-    def get_item_price(self, item_name):
-        item_price_xpath = self.item_price_checkout[1].format(item_name)
+    def get_item_price(self, product_name):
+        item_price_xpath = self.item_price_checkout[1].format(item_name=product_name)
         item_price = self.get_text(self.item_price_checkout[0], item_price_xpath)
-        """
-        manipular os dados daqui
-        """
-        return item_price
+        item_price_clean = ''
+
+        for char in item_price:
+            if char.isdigit() or char == '.':
+                item_price_clean += char
+
+        item_price_clean = float(item_price_clean)
+        return item_price_clean
+
+    def get_total_price_cart(self):
+        subtotal_element = self.find(*self.subtotal)
+        subtotal_value = subtotal_element.text
+        subtotal_value_clean = ''
+
+        for char in subtotal_value:
+            if char.isdigit() or char == '.':
+                subtotal_value_clean += char
+
+        return float(subtotal_value_clean)
 
     def click_to_finish_checkout(self):
         self.click(self.finish_checkout)
 
     def get_success_checkout_message(self):
-        self.get_text(self.success_checkout_message)
+        success_message_checkout = self.get_text(*self.success_checkout_message)
+        return success_message_checkout
