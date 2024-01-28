ab9d7c76a7bf4ab09d262ec253236cf65a964ef3
manuferreira <manussa.fsantos@gmail.com>
2024-01-28 10:55:16 -0300

    update initializer: typo

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,25 +1,33 @@
 from selenium.webdriver.common.by import By
 from pages.home_page import Homepage
 from utilities.locators import CartLocators
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CartPage(Homepage):
-    def __int__(self, webdriver):
-        super().__init__(webdriver)
-        self.remove_inside_cart_button = CartLocators.remove_product_button
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.remove_item_cart = CartLocators.remove_product_button
         self.checkout = (By.ID, 'checkout')
         self.continue_shopping = (By.ID, 'continue-shopping')
 
     def get_product_cart_title(self, index):
         self.get_product_title(index)
 
-    def remove_inside_cart(self, product_name):
-        remove_inside_cart_button_xpath = self.remove_inside_cart_button[1].format(product_name)
-        remove_inside_cart = self.find(self.remove_inside_cart_button[0], remove_inside_cart_button_xpath)
-        self.click(remove_inside_cart)
+    def delete_from_cart(self, product_name):
+        remove_item_cart_locator = self.remove_item_cart[1].format(item_name=product_name)
+        self.click((self.remove_item_cart[0], remove_item_cart_locator))
 
     def click_to_continue_shopping(self):
         self.click(self.continue_shopping)
 
     def click_to_checkout(self):
         self.click(self.checkout)
+
+    def item_not_present(self, index):
+        try:
+            self.get_product_cart_title(index)
+            return True
+
+        except Exception as e:
+            return False
