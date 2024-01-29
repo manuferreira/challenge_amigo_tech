0a16a4d0055919c5f1ae69c3c74632b2b617c14b
manuferreira <manussa.fsantos@gmail.com>
2024-01-28 21:04:35 -0300

    update some functions to use tuples

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,10 +4,9 @@ from utilities.locators import ProductsLocators
 
 
 class Homepage(BasePage):
-    def __int__(self, driver):
+    def __init__(self, driver):
         super().__init__(driver)
         self.product_title = (By.CLASS_NAME, 'inventory_item_name ')
-        #(//div[@class='inventory_item_name '])[index]
         self.product_description = (By.CLASS_NAME, 'inventory_item_desc')
         self.add_button = ProductsLocators.product_button
         self.remove_button = ProductsLocators.product_button
@@ -21,14 +20,12 @@ class Homepage(BasePage):
         return all_products_d[index].text
 
     def add_to_cart(self, product_name):
-        add_button_locator = self.add_button[1].format(product_name)
-        add_button = self.find(self.add_button[0], add_button_locator)
-        self.click(add_button)
+        add_button_locator = self.add_button[1].format(item_name=product_name)
+        self.click((self.add_button[0], add_button_locator))
 
     def remove_from_cart(self, product_name):
-        remove_button_locator = self.remove_button[1].format(product_name)
-        remove_button = self.find(self.remove_button[0], remove_button_locator)
-        remove_button.click()
+        remove_button_locator = self.remove_button[1].format(item_name=product_name)
+        self.click((self.remove_button[0], remove_button_locator))
 
 
 
