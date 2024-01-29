382117d3b4c352952a2c6e411aa86baa3952b03d
manuferreira <manussa.fsantos@gmail.com>
2024-01-28 21:03:27 -0300

    delete some code lines already in the initialize driver

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,10 @@
 import pytest
 from pages.login_page import LoginPage
-from utilities.test_data import TestData
 from conftest import initialize_driver
 
 @pytest.mark.usefixtures("initialize_driver")
 class TestLogin:
     def test_valid_credentials(self, initialize_driver):
         login_page = LoginPage(initialize_driver)
-        login_page.set_username(TestData.USERNAME)
-        login_page.set_password(TestData.PASSWORD)
-        login_page.click_to_login()
         actual_title = login_page.get_title()
         assert actual_title == "Swag Labs"
